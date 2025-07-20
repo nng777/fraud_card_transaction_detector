c9a928d5967f8d179257e88707b664ce1362caf5
nng777 <nikodemusng@gmail.com>
2025-07-20 10:37:04 +0700

    Commit

---
+++ detector.py
@@ -0,0 +1,8 @@
+"""
+1.Create python script credit_card_fraud_detection based on multiple unsupervised models for usual transactional fraud detection.
+2.Create another python script for "Menu" to:
+2.1.Train models based on input train_dataset.csv file
+2.2.Test models based on other test_dataset.csv file to detect fraud transaction
+2.3.Visualization of models comparison statistics save to image files with conclusion
+2.4.Exit
+"""
