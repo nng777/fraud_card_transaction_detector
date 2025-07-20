15e7deedbd17e5d08d41f8589e2ef050d7a21e87
nng777 <nikodemusng@gmail.com>
2025-07-20 10:57:50 +0700

    Commit

--- detector.py
+++ detector.py
@@ -2,7 +2,7 @@
 1.Create python script credit_card_fraud_detection based on multiple unsupervised models for usual transactional fraud detection.
 2.Create another python file for "Menu" to:
 2.1.Train models based on input train_dataset.csv file in the project directory, display training progress bar
-2.2.Test models based on other test_dataset.csv file to detect fraud transaction, display training progress bar and must 1. train model first to execute
+2.2.Test models based on other test_dataset.csv file to detect fraud transaction in the project directory, display training progress bar and must 1. train model first to execute
 2.3.Visualization of models comparison statistics save to image files with conclusion, must 1. train model first to execute
 2.4.Exit
 """
